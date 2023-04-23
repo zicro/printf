299984b833c3995dac4766b63fab772451d26453
ZHAYRI123 <merykozan@gmail.com>
2023-04-23 10:45:50 -0700

    update

--- main.h
+++ main.h
@@ -4,6 +4,19 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+#define UNUSED(x)(void)(x)
+
+/*FLAGS*/
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/*size*/
+#define S_LONG 2
+#define S_SHORT 1
+
 /**
  * struct put - structure for types printing
  * @t: type to print
@@ -14,6 +27,7 @@ typedef struct put
 	char *type;
 	int (*func)(va_list);
 } puts_list;
+
 
 int _putchar(char c);
 int _printf(const char *format, ...);
