917f99cc696efa2ba921fadae1f0e0c547fef4b4
Omae <larhnimikit@gmail.com>
2023-04-23 15:59:47 -0700

    re-check code

--- main.h
+++ main.h
@@ -4,19 +4,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-#define UNUSED(x)(void)(x)
-
-/*FLAGS*/
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
-
-/*size*/
-#define S_LONG 2
-#define S_SHORT 1
-
 /**
  * struct put - structure for types printing
  * @type: type to print
@@ -28,7 +15,6 @@ typedef struct put
 	int (*func)(va_list);
 } puts_list;
 
-int get_flags(const char *format, int *i);
 int _putchar(char c);
 int _printf(const char *format, ...);
 int put_char(va_list c);
